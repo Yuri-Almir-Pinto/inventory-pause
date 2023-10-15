97a2c8ba47593d39b3bd848bd265918d571083ff
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2023-10-15 19:25:28 -0300

    Downloaded a fabric template.

---
+++ src/client/java/com/yipeekiyaay/infiniteinventory/InfiniteInventoryClient.java
@@ -0,0 +1,10 @@
+package com.yipeekiyaay.infiniteinventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InfiniteInventoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/yipeekiyaay/infiniteinventory/InfiniteInventoryDataGenerator.java
@@ -0,0 +1,11 @@
+package com.yipeekiyaay.infiniteinventory;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InfiniteInventoryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
